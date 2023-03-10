66b2fb0ec7e2c1a87419dbb25b9b49d9f51d8bd2
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-03-10 18:33:01 -0300

    add calculate avg grade

--- main.js
+++ main.js
@@ -4,26 +4,64 @@ const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji aprovado" />';
 const imgReprovado =
   '<img src="./images/reprovado.png" alt="Emoji reprovado" />';
 
+const atividades = [];
+const notas = [];
+const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
+
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+
 const form = document.getElementById("form-atividade");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
 
+  adicionarLinha();
+  atualizarTabela();
+  atualizarMediaFinal();
+});
+
+function adicionarLinha() {
   const inputNomeAtividade = document.getElementById("nome-atividade");
   const inputNotaAtividade = document.getElementById("nota-atividade");
-  const corpoTabela = document.querySelector("tbody");
 
-  let linha = "<tr>";
-  linha += `<td>${inputNomeAtividade.value}</td>`;
-  linha += `<td>${inputNotaAtividade.value}</td>`;
-  linha += `<td>${
-    inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado
-  }</td>`;
-  linha += "</tr>";
+  if (atividades.includes(inputNomeAtividade.value)) {
+    alert(`A atividade ${inputNomeAtividade.value} já foi inserida`);
+  } else {
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
 
-  linhas += linha;
+    let linha = "<tr>";
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${
+      inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado
+    }</td>`;
+    linha += "</tr>";
 
-  corpoTabela.innerHTML = linhas;
+    linhas += linha;
+  }
 
   inputNomeAtividade.value = "";
   inputNotaAtividade.value = "";
-});
+}
+
+function atualizarTabela() {
+  const corpoTabela = document.querySelector("tbody");
+  corpoTabela.innerHTML = linhas;
+}
+
+function atualizarMediaFinal() {
+  const mediaFinal = calculaMediaFinal();
+  document.getElementById("media-final-valor").innerHTML = mediaFinal;
+  document.getElementById("media-final-resultado").innerHTML =
+    mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
+}
+
+function calculaMediaFinal() {
+  let somaDasNotas = 0;
+  for (let i = 0; i < notas.length; i++) {
+    somaDasNotas += notas[i];
+  }
+
+  return somaDasNotas / notas.length;
+}
