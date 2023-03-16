755976519e55d309c40f7f3b8c8ee636cf5841fc
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-03-16 20:48:57 -0300

    add: jquery manipulation

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("ok");
+$(document).ready(function () {
+  $("form").on("submit", function (e) {
+    e.preventDefault();
+
+    const taskList = $("#taskList");
+    const taskDescription = $("#txtTaskDescription").val();
+
+    const newTask = $(`<li>${taskDescription}</li>`);
+    newTask.appendTo(taskList);
+
+    $("li").click(function () {
+      $(this).addClass("taskCompleted");
+    });
+
+    $("#txtTaskDescription").val("");
+  });
+});
