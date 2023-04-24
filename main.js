3e2627648b66ee2b84ea2a307afd49fb629090d6
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-04-24 17:14:32 -0300

    conteúdo aula 3

--- main.js
+++ main.js
@@ -23,18 +23,35 @@ $(document).ready(function () {
     $(botao).find("i").addClass("d-none");
     $(botao).find("span").removeClass("d-none");
 
-    $.ajax(endpoint).done(function (resposta) {
-      const logradouro = resposta.logradouro;
-      const bairro = resposta.bairro;
-      const cidade = resposta.localidade;
-      const estado = resposta.uf;
-      const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+    // $.ajax(endpoint).done(function (resposta) {
+    //   const logradouro = resposta.logradouro;
+    //   const bairro = resposta.bairro;
+    //   const cidade = resposta.localidade;
+    //   const estado = resposta.uf;
+    //   const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
 
-      $("#endereco").val(endereco);
-      setTimeout(() => {
-        $(botao).find("i").removeClass("d-none");
-        $(botao).find("span").addClass("d-none");
-      }, 4000);
-    });
+    //   $("#endereco").val(endereco);
+    //   setTimeout(function () {
+    //     $(botao).find("i").removeClass("d-none");
+    //     $(botao).find("span").addClass("d-none");
+    //   }, 4000);
+    // });
+
+    fetch(endpoint)
+      .then(function (resposta) {
+        return resposta.json();
+      })
+      .then(function (json) {
+        const logradouro = json.logradouro;
+        const bairro = json.bairro;
+        const cidade = json.localidade;
+        const estado = json.uf;
+        const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+        $("#endereco").val(endereco);
+        setTimeout(function () {
+          $(botao).find("i").removeClass("d-none");
+          $(botao).find("span").addClass("d-none");
+        }, 4000);
+      });
   });
 });
