914ebbe2d58e31fbbd99f036d54da5c7e1edb6fa
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-04-24 17:30:58 -0300

    conteúdo aula 4

--- main.js
+++ main.js
@@ -48,10 +48,17 @@ $(document).ready(function () {
         const estado = json.uf;
         const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
         $("#endereco").val(endereco);
+      })
+      .catch(function (erro) {
+        alert(
+          "Ocorreu um erro ao buscar o endereço, tente novamente mais tarde."
+        );
+      })
+      .finally(function () {
         setTimeout(function () {
           $(botao).find("i").removeClass("d-none");
           $(botao).find("span").addClass("d-none");
-        }, 4000);
+        }, 1000);
       });
   });
 });
