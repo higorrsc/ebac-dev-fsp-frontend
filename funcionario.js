30366471456e96d12026e35935d31a716257d534
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-04-24 21:38:03 -0300

    conteúdo aula 5

---
+++ funcionario.js
@@ -0,0 +1,23 @@
+function Pessoa(nome) {
+  this.nome = nome;
+  this.dizOi = function () {
+    console.log(this.nome + " diz olá");
+  };
+}
+
+function Funcionario(nome, cargo, salario) {
+  this.cargo = cargo;
+  this.salario = salario;
+
+  this.dizCargo = function () {
+    console.log(this.cargo);
+  };
+
+  Pessoa.call(this, nome);
+}
+
+const funcionario1 = new Funcionario("Maria", "dev front-end", 5000);
+const pessoa = new Pessoa("Gian");
+
+funcionario1.dizOi();
+funcionario1.dizCargo();
