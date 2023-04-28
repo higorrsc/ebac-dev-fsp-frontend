45057d55702ffcd80264e115aa8821906e778162
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-04-28 17:44:21 -0300

    conteúdo aula 8

---
+++ src/dom.ts
@@ -0,0 +1,5 @@
+const campoNome = document.getElementById("nome");
+const formulario = document.getElementsByTagName("form");
+const botaoEnviar = document.getElementById("btn-enviar");
+
+botaoEnviar?.addEventListener("click", (e: MouseEvent) => {});
