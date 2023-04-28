a465b55b39247d3a5a1031328837d8aaf3c7bfe1
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-04-28 07:18:42 -0300

    conteúdo aula 4

---
+++ src/tipos_basicos.js
@@ -0,0 +1,22 @@
+"use strict";
+let estaChovendo = false;
+estaChovendo = true;
+let idade = 27;
+let altura = 1.75;
+const nacionalidade = "brasileira";
+const colegas = ["lucas", "fernanda"];
+const tecnologias = ["html", "css", "js"];
+const notas = [7, 9, 5, 8];
+const lista = [
+    "higor",
+    true,
+    39,
+];
+let idadeDaAna = 25;
+idadeDaAna = "25 anos";
+let dadosDaApi;
+dadosDaApi = 10;
+dadosDaApi = true;
+dadosDaApi = [1, 2, 3];
+dadosDaApi = "string";
+let curso = "front-end";
