52dea0f782afc169d9f3cb8a7ce0a39107f037b3
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-05-17 18:24:14 -0300

    aula 05

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/higorrsc.png" alt="" />
+    <Titulo>Higor Cruz</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident
+      praesentium ipsam, cum nulla et tempore fuga ex tempora incidunt. Quidem,
+      doloribus sunt? Reiciendis ut vel veniam necessitatibus cupiditate,
+      placeat autem?
+    </p>
+  </section>
+)
+
+export default Sobre
