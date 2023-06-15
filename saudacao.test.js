80a258e0457921e3875165864995878fcd24899d
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2023-06-15 07:40:10 -0300

    conteúdo aula 03

--- saudacao.test.js
+++
@@ -1,8 +0,0 @@
-const saudacao = require('./saudacao')
-
-describe('Testes para saudação', () => {
-    test('Deverá retornar Olá, Maria', () => {
-        const olaMaria = saudacao.dizOla('Maria')
-        expect(olaMaria).toBe('Olá, Maria')
-    })
-})
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+function App() {
+  const [tarefas, setTarefas] = useState([]);
+  const [tarefaTemp, setTarefaTemp] = useState("");
+
+  function casdastrarTarefa() {
+    setTarefas([...tarefas, tarefaTemp]);
+    setTarefaTemp("");
+  }
+
+  return (
+    <div className="App">
+      <input
+        data-testid="campo-tarefa"
+        type="text"
+        value={tarefaTemp}
+        onChange={(evento) => setTarefaTemp(evento.target.value)}
+      />
+      <button
+        data-testid="btn-cadastrar"
+        type="button"
+        onClick={casdastrarTarefa}>
+        cadastrar
+      </button>
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
